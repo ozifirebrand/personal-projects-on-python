bef7ab5430e72f92ef6f9fc0fe970740573c8e7a
Ozioma <oziomaokoroafor@gmail.com>
2021-08-18 16:46:48 +0100

    Functions and conditionals

--- classWorksAndPractice/DeitelPractices/chapter3/conditionals.py
+++ classWorksAndPractice/DeitelPractices/chapter3/conditionals.py
@@ -1,12 +1,33 @@
-# grade = 85
-# if grade >= 60:
-#     print('Passed')
-#
-#     grade = 55
-#     if grade >= 60:
-# print('Passed')
-grade = 57
-if grade >= 60:
-    print('Passed')
-else:
-    print('Failed')
+def grade_checker():
+    grades = int(input("Number: "))
+    if grades >= 60:
+        print('Passed')
+
+
+def grade_checker_2():
+    grades = int(input("Number: "))
+    if grades >= 60:
+        print('Passed')
+
+
+def grade_checker_3():
+    grade = int(input("Number: "))
+    if grade >= 60:
+        print('Passed')
+    else:
+        print('Failed')
+
+
+def grade_checker_4():
+    grade = int(input("Number: "))
+    if grade >= 60:
+        result = 'Passed'
+    else:
+        result = 'Failed'
+        print(result)
+
+
+grade_checker()
+grade_checker_2()
+grade_checker_3()
+grade_checker_4()
