4c898107223822052f03bd4c542cb990e5e50ea5
Ozioma <oziomaokoroafor@gmail.com>
2021-08-25 11:50:43 +0100

    Finding the area and diameter of a circle

---
+++ classWorksAndPractice/DeitelPractices/chapter_2/circle_area_and_diameter.py
@@ -0,0 +1,11 @@
+pi = 3.14159
+circle_radius = float(input("What is circle's radius in cm?: "))
+diameter = 2 * circle_radius
+area_of_circle = pi * circle_radius ** 2
+circumference_of_circle = 2 * pi * circle_radius
+
+print('Circle radius is', circle_radius, 'cm')
+print('Circle diameter is', diameter, 'cm')
+print('Circle area is', area_of_circle, 'cm')
+print('Circle circumference is', circumference_of_circle, 'cm')
+
