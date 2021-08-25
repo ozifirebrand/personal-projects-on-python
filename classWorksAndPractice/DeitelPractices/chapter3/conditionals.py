1d3c8c2fcb79e2a7486f944b96e5fa0796580530
Ozioma <oziomaokoroafor@gmail.com>
2021-08-25 10:59:23 +0100

    PersonalProjects deletions commit

--- classWorksAndPractice/DeitelPractices/chapter3/conditionals.py
+++ classWorksAndPractice/DeitelPractices/chapter3/conditionals.py
@@ -1,21 +1,21 @@
-def grade_checker():
-    grades = int(input("Number: "))
-    if grades >= 60:
-        print('Passed')
+# def grade_checker():
+#     grades = int(input("Number: "))
+#     if grades >= 60:
+#         print('Passed')
+#
+#
+# def grade_checker_2():
+#     grades = int(input("Number: "))
+#     if grades >= 60:
+#         print('Passed')
 
 
-def grade_checker_2():
-    grades = int(input("Number: "))
-    if grades >= 60:
-        print('Passed')
-
-
-def grade_checker_3():
-    grade = int(input("Number: "))
-    if grade >= 60:
-        print('Passed')
-    else:
-        print('Failed')
+# def grade_checker_3():
+#     grade = int(input("Number: "))
+#     if grade >= 60:
+#         print('Passed')
+#     else:
+#         print('Failed')
 
 
 def grade_checker_4():
@@ -24,10 +24,10 @@ def grade_checker_4():
         result = 'Passed'
     else:
         result = 'Failed'
-        print(result)
+    print(result)
 
 
-grade_checker()
-grade_checker_2()
-grade_checker_3()
+# grade_checker()
+# grade_checker_2()
+# grade_checker_3()
 grade_checker_4()
