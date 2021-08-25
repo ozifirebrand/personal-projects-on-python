640df3eafeeabd78f19ea4029870cbcb7beae6bb
Ozioma <oziomaokoroafor@gmail.com>
2021-08-25 11:32:09 +0100

    table of squares refactored

--- classWorksAndPractice/DeitelPractices/chapter_2/table_of_squares_and_cubes.py
+++ classWorksAndPractice/DeitelPractices/chapter_2/table_of_squares_and_cubes.py
@@ -5,3 +5,20 @@ print('2  \t\t', 2 ** 2, '\t\t\t', 2 ** 3)
 print('3  \t\t', 3 ** 2, '\t\t\t', 3 ** 3)
 print('4  \t\t', 4 ** 2, '\t\t', 4 ** 3)
 print('5  \t\t', 5 ** 2, '\t\t', 5 ** 3)
+
+# Using for loops
+# Each of the values are incremented by 1
+# The square and cubes of the values are computed
+print()
+print()
+
+number = 0
+print('number \t square \t cube')
+while number <= 3:
+    print(number, '\t\t', number ** 2, '\t\t\t', number ** 3)
+    number += 1
+
+number = 4
+while number <= 5:
+    print(number, '\t\t', number ** 2, '\t\t', number ** 3)
+    number += 1
