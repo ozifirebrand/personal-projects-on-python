333db2036ba19057ef5aba7bd102c2da65afe38a
Ozioma <oziomaokoroafor@gmail.com>
2021-08-31 20:48:28 +0100

    Challenging Kelechi Divine commit

---
+++ PacktExercises/iterations_and_selection.py
@@ -0,0 +1,13 @@
+for i in 'Portland':
+    print(i, end=' ')
+
+# For the first bot, the steps are as follows:
+    # Ask the user at least two questions.
+    # Respond to each answer. Include the answer in the response.
+
+# For the second bot, the steps are as follows:
+#
+#     Ask a question that can be answered with a number scale, such as "On a scale of 1-10…".
+#     Respond differently depending on the answer given.
+#     State a different question following each answer that can be answered with a number scale.
+#     Respond differently depending on the answer given.
---
+++ classWorksAndPractice/challenge_question.py
@@ -0,0 +1,12 @@
+def set_values():
+    part_number = input()
+    price_per_item = int(input())
+    part_description = input()
+    quantity_of_item_purchased = int(input())
+
+
+def get_value():
+    print(set_values())
+
+
+get_value()
