409fe499bd7f7e703ae75a3fc4c76467630caf9e
Ozioma <oziomaokoroafor@gmail.com>
2021-08-31 13:39:30 +0100

    Personal projects commit

---
+++ PacktExercises/using_continue_and_break.py
@@ -0,0 +1,18 @@
+for num in range(10, 100):
+    if num % 2 == 0:
+        continue
+    if num % 3 == 0:
+        continue
+    if num % 5 == 0:
+        continue
+    if num % 7 == 0:
+        continue
+    print(num)
+
+for bae in range(1, 10):
+    # if bae > 2:
+    #     print(bae)
+    #     continue
+    if bae < 9:
+        print(bae)
+""" wahala be like bicycle!"""
