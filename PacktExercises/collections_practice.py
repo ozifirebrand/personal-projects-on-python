0c67759492cd625437654ca637057c6f5a62cc47
Ozioma <oziomaokoroafor@gmail.com>
2021-09-01 21:20:11 +0100

    Collections

--- PacktExercises/collections_practice.py
+++ PacktExercises/collections_practice.py
@@ -1,6 +1,14 @@
+#
+# m = [[1, 2, 3], [4, 5, 6]]
+# for row in range(len(m)):
+#     for column in range(len(m[row])):
+#         print(m[row][column], end=' ')
+#     print()
+# print()
 
-m = [[1, 2, 3], [4, 5, 6]]
-for row in range(len(m)):
-    for column in range(len(m[row])):
-        print(m[row][column], end=' ')
+company_data = [["Name", "Age", "Department"], ["John Mckee", 38, "Sales"],
+                ["Lisa Crawford", 29, "Marketing"], ["Sujan Patel", 33, "HR"]]
+for item in range(len(company_data)):
+    for list_array in range(len(company_data[item])):
+        print(company_data[item][list_array], end=' ')
     print()
