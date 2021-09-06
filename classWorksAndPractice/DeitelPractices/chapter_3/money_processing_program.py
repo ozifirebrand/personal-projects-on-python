1b8fff7a2181d17ca40a013a4c7f6e993c17f553
Ozioma <oziomaokoroafor@gmail.com>
2021-09-06 09:11:40 +0100

    Money processing program commit 2

--- classWorksAndPractice/DeitelPractices/chapter_3/money_processing_program.py
+++ classWorksAndPractice/DeitelPractices/chapter_3/money_processing_program.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 
-principal = Decimal('1000.00')
-print(principal)
-rate = Decimal('0.05')
-print(rate)
+# principal = Decimal('1000.00')
+# print(principal)
+# rate = Decimal('0.05')
+# print(rate)
 # A person invests $1000 in a savings account yielding 5% interest. Assuming that
 # the person leaves all interest on deposit in the account, calculate and display the
 # amount of money in the account at the end of each year for 10 years. Use the following
@@ -14,4 +14,9 @@ print(rate)
 # r is the annual interest rate,
 # n is the number of years and
 # a is the amount on deposit at the end of the nth year.
-
+#
+principal = 1000.00
+rate = 0.05
+for year in range(1, 11):
+    amount = principal * (1 + rate) ** year
+    print(f'{year:.2}{amount:>10.2f}')
