8ff8af350abbc902d20cfbf1f8e46469d4daad14
Ozioma <oziomaokoroafor@gmail.com>
2021-09-07 16:50:05 +0100

    Added other conditions commit

--- classWorksAndPractice/DeitelPractices/chapter_3/tax_on_bill_program.py
+++ classWorksAndPractice/DeitelPractices/chapter_3/tax_on_bill_program.py
@@ -3,4 +3,18 @@
 # total, then print the result with two digits to the right of the
 # decimal point.
 from decimal import Decimal
+
 print(f"{Decimal('37.45') * Decimal('1.0625'):.2f}")
+
+# COMPARISON OF VALUES
+i, j, k, m = 1, 2, 3, -2
+var = i >= 1 and j < 4
+print(var)
+var = 99 >= m > k
+print(var)
+var = j >= i or k == m
+print(var)
+var = (k+m < j) or (3-j>=k)
+print(var)
+var = not (k>m)
+print(var)
