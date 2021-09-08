58e20c4d5eacc69fd7eeb78cac8f187f89e00ddc
Ozioma <oziomaokoroafor@gmail.com>
2021-09-08 23:01:54 +0100

    Craps program

---
+++ classWorksAndPractice/DeitelPractices/chapter_4/craps_game.py
@@ -0,0 +1,7 @@
+# die one and two are rolled
+# the sum of the two faces determines if player wins, loses or continues
+# game
+# A sum of 7 or 11 means win, the sum is 2, 3 or 12 on the first roll (called “craps”), you lose (i.e., the “house”
+# wins). If the sum is 4, 5, 6, 8, 9 or 10 on the first roll, that sum becomes your
+# “point.” To win, you must continue rolling the dice until you “make your point”
+# (i.e., roll that same point value). You lose by rolling a 7 before making your point.
