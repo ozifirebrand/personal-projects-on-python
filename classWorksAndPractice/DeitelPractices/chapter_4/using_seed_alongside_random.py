19b4318b18c81c156876a086ba0308be518602bc
Ozioma <oziomaokoroafor@gmail.com>
2021-09-08 21:44:33 +0100

    coin tossing commit

--- classWorksAndPractice/DeitelPractices/chapter_4/using_seed_alongside_random.py
+++ classWorksAndPractice/DeitelPractices/chapter_4/using_seed_alongside_random.py
@@ -1,23 +1,31 @@
 import random
 
-random.seed(32)
-for roll in range(10):
-    print(random.randrange(1, 7), end=' ')
-
-print()
-for roll in range(10):
-    print(random.randrange(1, 7), end=' ')
-
-print()
-
-random.seed(32)
-
-for roll in range(10):
-    print(random.randrange(1, 7), end=' ')
-
-print()
-
 # random.seed(32)
+# for roll in range(10):
+#     print(random.randrange(1, 7), end=' ')
+#
+# print()
+# for roll in range(10):
+#     print(random.randrange(1, 7), end=' ')
+#
+# print()
+#
+# random.seed(32)
+#
+# for roll in range(10):
+#     print(random.randrange(1, 7), end=' ')
 
-for roll in range(10):
-    print(random.randrange(1, 7), end=' ')
+# print()
+#
+# # random.seed(32)
+#
+# for roll in range(10):
+#     print(random.randrange(1, 7), end=' ')
+#
+
+# frequency_h = 0
+# frequency_t = 0
+
+for i in range(20):
+    print('H' if random.randrange(2) == 0 else 'T', end=' ')
+
