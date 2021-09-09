421754c91c48d7919a5d906525e1ace6a4141a8b
Ozioma <oziomaokoroafor@gmail.com>
2021-09-09 12:50:32 +0100

    Added volume and input methods in the program

--- classWorksAndPractice/DeitelPractices/chapter_4/area_program.py
+++ classWorksAndPractice/DeitelPractices/chapter_4/area_program.py
@@ -1,7 +1,17 @@
-def rectangle_area(breadth, length=2, width=3):
-    breadth = 3*length
-    print(breadth)
-    return length * width
+def rectangle_area(length_self, width):
+    return length_self * width
 
 
-print(rectangle_area(7, width=4, length=6,))
+def cuboid_volume(length_self, breadth_self, height_self):
+    return length_self * breadth_self * height_self
+
+
+length_of_rectangle = int(input('What is the length of the rectangle? '))
+breadth_of_rectangle = int(input('What is the breadth of the rectangle? '))
+print(rectangle_area(length_of_rectangle, breadth_of_rectangle))
+
+
+length = int(input('What is the length of the cuboid? '))
+breadth = int(input('What is the breadth of the cuboid? '))
+height = int(input('What is the height of the cuboid? '))
+print(cuboid_volume(length, breadth, height))
