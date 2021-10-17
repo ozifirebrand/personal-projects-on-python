317c704a8ea1b76b591841b01eb3a12d125eb59c
Ozioma <oziomaokoroafor@gmail.com>
2021-10-17 18:17:18 +0100

    Working with dictionary and lists refactored

---
+++ PacktExercises/working_with_dictionary_and_list.py
@@ -0,0 +1,11 @@
+country_codes = {'Finland': 'fi',
+                'South Africa': 'za',
+                'Nepal': 'np'}
+
+for value in country_codes.keys():
+    print(value)
+
+
+shopping = ['Apple', 'Eggs', 'Yam', 'Boli', 'Tuwo']
+
+print(shopping)
