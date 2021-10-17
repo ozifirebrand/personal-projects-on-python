4e1625f6c95d65ca062c5a6b5bc59d695608067f
Ozioma <oziomaokoroafor@gmail.com>
2021-10-17 19:00:27 +0100

    Different means of working with dictionaries and lists

--- PacktExercises/working_with_dictionary_and_list.py
+++ PacktExercises/working_with_dictionary_and_list.py
@@ -21,3 +21,24 @@ employee = {
 }
 
 print(employee)
+
+movie = {
+    'title' : 'The Godfather',
+    'director' : 'Francis Ford Coppola',
+    'year' : 1972,
+    'rating' : 9.2
+}
+
+print(movie['year'])
+print(movie['title'])
+print(movie['director'])
+print(movie['rating'])
+print(movie)
+
+movie_2 = {}
+movie_2['title'] = 'Abejoye'
+movie_2['year'] = 2021
+movie_2['director'] = 'Mike Bamiloye'
+movie_2['rating'] = 9.0
+
+print(movie_2)
