023400b01212d9b4c3029de57b8d39f01211a882
Ozioma <oziomaokoroafor@gmail.com>
2021-10-19 19:14:25 +0100

    Committing the works in the personal projects folder

--- shopping/testItem.py
+++ shopping/testItem.py
@@ -10,6 +10,3 @@ class TestItem(TestCase):
     def test_to_string(self):
         item = Item("Puff puff", 5, 2)
         self.assertEqual("Puff puff			5			2", item.__str__(), "Not true")
-
-
-        # %-3s
