622a720f466643d09faf18cc0b012537f749a502
Ozioma <oziomaokoroafor@gmail.com>
2021-11-06 21:22:57 +0100

    Finally did this exercise

--- classWorksAndPractice/DeitelPractices/chapter_5/summarize_letters.py
+++ classWorksAndPractice/DeitelPractices/chapter_5/summarize_letters.py
@@ -1,5 +1,12 @@
-def summarise_letters(word):
-    for char in word:
-        list(enumerate(char))
-    pass
+def count_characters(characters):
+    a_tuple = ()
+    a_list = []
+    for char in characters:
+        if char in a_list:
+            continue
+        else:
+            a_list.append(char)
+            a_tuple = tuple(a_list)
+    return a_tuple
 
+print(count_characters("Sherifat Omo logba logba"))
