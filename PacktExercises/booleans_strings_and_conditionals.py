199a5cf16326e0eef86d187a5bd19efd9bd87245
Ozioma <oziomaokoroafor@gmail.com>
2022-01-21 14:09:04 +0100

    Sliced strings, compared and used conditionals

--- PacktExercises/booleans_strings_and_conditionals.py
+++ PacktExercises/booleans_strings_and_conditionals.py
@@ -21,3 +21,26 @@ print(over_18 and over_21)
 print(over_18 or over_21)
 print(not over_21 or (over_18 or over_21))
 print(not over_18)
+
+new_york = "New York"
+san_francisco = "San Francisco"
+print("\n")
+print(new_york > san_francisco)
+print(new_york < san_francisco)
+
+print("\n")
+ability = "abilities"
+print(ability[1:5])
+print(ability[:5])
+print(ability[-1:])
+print(ability[:-2])
+print(ability[-5:])
+
+print("\n")
+age = 18
+
+if 18 <= age < 21:
+    print("At least you can vote")
+    print("Poker will have to wait")
+    if age == 18:
+        print("Perfect!")
