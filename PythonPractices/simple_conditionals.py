abe742a96189705de1c0c15b6cee0343ad191003
Ozioma <oziomaokoroafor@gmail.com>
2022-03-04 18:05:44 +0100

    No comment

---
+++ PythonPractices/simple_conditionals.py
@@ -0,0 +1,9 @@
+print("What is your name?")
+name = input()
+if name == "Ozi":
+    print("Welcome Ozi")
+elif name == "Toye":
+    print("Welcome Toye")
+else:
+    print("Welcome ", name)
+    print("Welcome alaye {}" .format(name))
