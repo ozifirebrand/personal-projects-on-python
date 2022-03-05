954f591b26e37be3d10cce13f6220cfe6750aea2
Ozioma <oziomaokoroafor@gmail.com>
2022-03-05 01:29:31 +0100

    A commit

--- PythonPractices/lists_and_dicts.py
+++ PythonPractices/lists_and_dicts.py
@@ -4,3 +4,8 @@ print(new_items)
 stuff = {"my_world": "GOD", "my_name": "Martha"}
 new_items.update(stuff)
 print(new_items)
+new_items.update(food="money")
+print(new_items)
+
+new_items.pop("food")
+print(new_items)
